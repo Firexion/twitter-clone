cf1adc8d93e9a7efb7593d9e8fb5d3baac1d6093
Sasha Perdue <firexion@gmail.com>
2016-06-27 10:38:48 -0400

    Adding in mongo

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-import koa from 'koa';
+import koa from 'koa'
 import serve from 'koa-static'
+import {MongoClient} from 'mongodb'
 
-const app = new koa();
+const APP_PORT = 3000;
 
-app.use(serve(__dirname + '/dist'));
 
-app.listen(3000);
+(async () => {
 
-console.log('listening on port 3000');
+		// Connect to the db
+		const db = await MongoClient.connect("mongodb://localhost:27017/twitter-react");
+
+		const users = await db.collection("users").find({}).toArray();
+		console.log(users);
+
+
+		// Serve static resources
+		const app = new koa();
+
+		app.use(serve(__dirname + '/dist'));
+
+		app.listen(APP_PORT, () => {
+		  console.log(`App is now running on http://localhost:${APP_PORT}`);
+		});
+})();
